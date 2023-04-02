220342c92b3678d3e606c0a04f50fbb415e0396d
tetelevm <tetelev99@mail.ru>
2023-04-03 01:17:59 +0300

    Added README

--- config.py
+++ config.py
@@ -9,6 +9,11 @@ from pathlib import Path
 from typing import Final, Tuple
 
 
+VERSION: Final[str] = "0.5"
+# VERSION: Final[str] = "0.7"  # + add a deletion check and testing
+# VERSION: Final[str] = "1.0"  # + add tower storing in memcache
+
+
 def _get_args() -> dict:
     """
     Reads envs from the file in the root of the project.
@@ -24,17 +29,11 @@ def _get_args() -> dict:
 
 _args = _get_args()
 
-
-VERSION: Final[str] = "0.49"
-# VERSION: Final[str] = "0.5"  # create README
-# VERSION: Final[str] = "0.7"  # + add a deletion check and testing
-# VERSION: Final[str] = "1.0"  # + add tower storing in memcache
+TOKEN = _args["TOKEN"]
+BOT_USERNAME = _args["BOT_USERNAME"]
 
 
 TOWER: Final[str] = "ITSWEDNESDAYMYDUDES!"
 CRASH_LENS: Tuple[int, ...] = (3, 14, 12, 7, 3)
 MINIMAL_CHECK_LEN: Final[int] = 3
 WEDNESDAY_MODE: Final[bool] = False
-
-TOKEN = _args["TOKEN"]
-BOT_USERNAME = _args["BOT_USERNAME"]
